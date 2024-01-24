d76aff6e858498cf0db9b4fe94c625288052ee22
PokeFred <Cedric.paelmke@gmx.de>
2024-01-24 14:58:02 +0100

    update tauri commands

--- src/app.svelte
+++ src/app.svelte
@@ -1,1 +1,16 @@
-<div>Hello World</div>
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api/tauri"
+
+    let name: string = ""
+    let greetMsg: string = ""
+
+    async function greet() {
+        greetMsg = await invoke("greet", { name })
+    }
+</script>
+
+<div>
+    <input type="text" bind:value={name}>
+    <button on:click={greet}>Send</button>
+</div>
+<div>{greetMsg}</div>
