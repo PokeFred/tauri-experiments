363f9ecdcc268568d7522bba61c01111a59b9988
PokeFred <Cedric.paelmke@gmx.de>
2024-01-24 14:40:04 +0100

    init svelte

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite"
+import type { CommonServerOptions } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+
+const serverConfig: CommonServerOptions = {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true
+}
+
+export default defineConfig({
+    base: "/",
+    server: serverConfig,
+    preview: serverConfig,
+    build: {
+        outDir: "./build"
+    },
+    plugins: [svelte()]
+})
